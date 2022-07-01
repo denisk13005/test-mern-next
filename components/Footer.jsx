0a538391f07cbb4d5299b002c1669523c082c924
denisk13005 <kirmanndenis@gmail.com>
2022-07-01 15:49:16 +0200

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
 import React from "react";
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" layout="fill" alt="" objectFit="cover" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h1 className={styles.title}>blabla</h1>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>adresse</h1>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>contact</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
