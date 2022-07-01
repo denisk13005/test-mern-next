459c4b7582676aa42b4eda1d29560b47c9fbf41d
denisk13005 <kirmanndenis@gmail.com>
2022-07-01 15:34:04 +0200

    composant pizzaList et pizza card

---
+++ components/PizzaList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
+
+const PizzaList = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN MASSALIA </h1>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        necessitatibus velit eveniet iure, repellendus corrupti animi quos
+        itaque expedita ratione!
+      </p>
+      <div className={styles.wrapper}>
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
