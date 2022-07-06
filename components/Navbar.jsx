2e023c503e677162d50b6d52bca6856ca96aad58
denisk13005 <kirmanndenis@gmail.com>
2022-07-06 17:23:31 +0200

    link sur cart

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import styles from "../styles/Navbar.module.css";
 import { useSelector } from "react-redux";
+import Link from "next/link";
 
 const Navbar = () => {
    const quantity = useSelector((state) => state.cart.quantity);
@@ -33,12 +34,21 @@ const Navbar = () => {
                <li className={styles.listItem}>Contact</li>
             </ul>
          </div>
-         <div className={styles.item}>
-            <div className={styles.cart}>
-               <Image src="/img/cart.png" alt="" width="30px" height="30px" />
-               <div className={styles.counter}>{quantity}</div>
-            </div>
-         </div>
+         <Link href="/cart">
+            <a>
+               <div className={styles.item}>
+                  <div className={styles.cart}>
+                     <Image
+                        src="/img/cart.png"
+                        alt=""
+                        width="30px"
+                        height="30px"
+                     />
+                     <div className={styles.counter}>{quantity}</div>
+                  </div>
+               </div>
+            </a>
+         </Link>
       </div>
    );
 };
