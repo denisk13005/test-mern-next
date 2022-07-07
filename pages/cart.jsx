d5fe4350ad4fc28f60ed9e11c904be050b35dad0
denisk13005 <kirmanndenis@gmail.com>
2022-07-07 10:53:48 +0200

    ajout de la méthode de payment paypal

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -2,11 +2,82 @@ import styles from "../styles/Cart.module.css";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+   PayPalScriptProvider,
+   PayPalButtons,
+   usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
 
 const Cart = () => {
+   //state
    const state = useSelector((state) => state.cart);
-   console.log(state);
    const [totalPrice, setTotalPrice] = useState(0);
+
+   //to open the paypal payment btn after click on checkout now
+   const [open, setOpen] = useState(false);
+   //paypal
+   const amount = totalPrice;
+   const currency = "USD";
+   const style = { layout: "vertical" };
+   console.log(open);
+
+   // Custom component to wrap the PayPalButtons and handle currency changes
+   const ButtonWrapper = ({ currency, showSpinner }) => {
+      // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+      // This is the main reason to wrap the PayPalButtons in a new component
+      const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+      useEffect(() => {
+         dispatch({
+            type: "resetOptions",
+            value: {
+               ...options,
+               currency: currency,
+            },
+         });
+      }, [currency, dispatch]);
+
+      return (
+         <>
+            {showSpinner && isPending && <div className="spinner" />}
+            <PayPalButtons
+               style={style}
+               disabled={false}
+               forceReRender={[amount, currency, style]}
+               fundingSource={undefined}
+               createOrder={(data, actions) => {
+                  return actions.order
+                     .create({
+                        purchase_units: [
+                           {
+                              amount: {
+                                 currency_code: currency,
+                                 value: amount,
+                              },
+                           },
+                        ],
+                     })
+                     .then((orderId) => {
+                        // Your code here after create the order
+                        return orderId;
+                     });
+               }}
+               onApprove={function (data, actions) {
+                  return actions.order.capture().then(function (details) {
+                     console.log(details);
+                     // const shipping = details.purchase_units[0].shipping;
+                     // createOrder({
+                     //    customer: shipping.name.full_name,
+                     //    address: shipping.address.address_line_1,
+                     //    total: cart.total,
+                     //    method: 1,
+                     // });
+                  });
+               }}
+            />
+         </>
+      );
+   };
 
    // eslint-disable-next-line react-hooks/exhaustive-deps
    const calculTotalPrice = () => {
@@ -97,7 +168,34 @@ const Cart = () => {
                <div className={styles.totalText}>
                   <b className={styles.totalTextTitle}>Total:</b>${totalPrice}
                </div>
-               <button className={styles.button}>CHECKOUT NOW!</button>
+               {open ? (
+                  <div className={styles.paymentMethods}>
+                     <button className={styles.payButton}>
+                        Paiement à la réception
+                     </button>
+                     <PayPalScriptProvider
+                        options={{
+                           "client-id":
+                              "Adm4k9ktEzorAxcfVNlQCQ7USWKmD5bhkDMPM_ti-btU8xCCklpdWv6DGDMCM7qY9r9oQOEaf90JWlGq",
+                           components: "buttons",
+                           currency: "USD",
+                           "disable-funding": "credit,card,p24", //désactive les méthodes de paiement citées
+                        }}
+                     >
+                        <ButtonWrapper
+                           currency={currency}
+                           showSpinner={false}
+                        />
+                     </PayPalScriptProvider>
+                  </div>
+               ) : (
+                  <button
+                     className={styles.button}
+                     onClick={() => setOpen(true)}
+                  >
+                     CHECKOUT NOW!
+                  </button>
+               )}
             </div>
          </div>
       </div>
